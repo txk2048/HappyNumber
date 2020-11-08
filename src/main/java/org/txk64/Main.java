193f594c05165e5784aa8a3b935cb641bf4f1c73
txk64 <38069888+txk64@users.noreply.github.com>
2020-11-08 16:28:58 +0000

    Add UI code

--- src/main/java/org/txk64/Main.java
+++ src/main/java/org/txk64/Main.java
@@ -1,8 +1,19 @@
 package org.txk64;
 
 
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+            throws ClassNotFoundException, UnsupportedLookAndFeelException,
+                   InstantiationException, IllegalAccessException {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow.display();
+            }
+        });
     }
 }
---
+++ src/main/java/org/txk64/MainWindow.java
@@ -0,0 +1,58 @@
+package org.txk64;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainWindow extends JFrame {
+    private final JLabel numberSelectorLabel;
+    private final JSpinner numberSelector;
+
+    private final JLabel isHappyDisplayLabel;
+    private final JTextField isHappyDisplayField;
+
+    private MainWindow() {
+        // window setup code
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        setSize(300, 300);
+        setTitle("HappyNumber");
+
+        // setup UI components
+        numberSelectorLabel = new JLabel("Number: ");
+        numberSelector = new JSpinner(new SpinnerNumberModel(1, 1, Integer.MAX_VALUE, 1));
+
+        isHappyDisplayLabel = new JLabel("Is happy?: ");
+        isHappyDisplayField = new JTextField();
+
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(10, 10, 5, 5);
+        constraints.weightx = 0.0d;
+        constraints.weighty = 0.5d;
+        add(numberSelectorLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.insets = new Insets(10, 5, 5, 10);
+        constraints.weightx = 1.0d;
+        add(numberSelector, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(5, 10, 10, 5);
+        constraints.weightx = 0.0d;
+        add(isHappyDisplayLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.insets = new Insets(5, 5, 10, 10);
+        constraints.weightx = 1.0d;
+        add(isHappyDisplayField, constraints);
+    }
+
+    public static void display() {
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+    }
+}
